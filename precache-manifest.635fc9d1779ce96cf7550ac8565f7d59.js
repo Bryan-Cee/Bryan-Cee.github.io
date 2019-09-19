bb03bf1ed353f2d23b29bb4dd731103f3afee98c
Bryan-Cee <cheruiyotbryan@gmail.com>
2019-09-19 22:38:01 +0300

    Updates

---
+++ precache-manifest.635fc9d1779ce96cf7550ac8565f7d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d0ac25c3b35cf983cf5",
+    "url": "/static/js/main.d76fb66f.chunk.js"
+  },
+  {
+    "revision": "f70d70835a07d077090c",
+    "url": "/static/js/2.8045db70.chunk.js"
+  },
+  {
+    "revision": "5d0ac25c3b35cf983cf5",
+    "url": "/static/css/main.6d9468bf.chunk.css"
+  },
+  {
+    "revision": "f70d70835a07d077090c",
+    "url": "/static/css/2.4af1e92b.chunk.css"
+  },
+  {
+    "revision": "eba2318b194f9efd4328fdebe229cc87",
+    "url": "/index.html"
+  }
+];
