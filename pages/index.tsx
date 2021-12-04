e7462f3df282d2dd1751d6c4e47e5a03d10337ee
Bryan-Cee <cheruiyotbryan@gmail.com>
2021-12-04 18:03:04 +0300

    change portfolio design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,54 @@
+import React from 'react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+
+import Nav from '../components/Nav';
+
+const icons = [
+  {
+    icon: 'github.svg',
+    label: 'github',
+  },
+  {
+    icon: 'twitter.svg',
+    label: 'twitter',
+  },
+  {
+    icon: 'linkedin.svg',
+    label: 'linkedin',
+  },
+];
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <nav className={styles.nav}>
-        <li>About</li>
-        <li>Blog</li>
-        <li>Contact</li>
-      </nav>
-      <main className={styles.main}>
+    <div className=" bg-[#0077b6] min-h-screen flex flex-col h-screen">
+      <Nav />
+      <main className="flex items-center justify-center h-screen">
         <section>
           <div>
-            <p className={styles.info}>Helloo</p>
-            <p className={styles.name}>I'm Bryce</p>
-            <p className={styles.title}>Software Engineer @Microsoft</p>
+            <p className="text-5xl leading-[1.5] font-bold m-0">Hello</p>
+            <p className="font-black text-[10rem] text-[aqua] m-0 leading-tight">
+              I'm Bryce
+            </p>
+            <p className="text-5xl leading-normal font-bold m-0">
+              Software Engineer @Microsoft
+            </p>
           </div>
-          <div className={styles.socials}>
-            <img className={styles.logo} src="github.svg" alt="github" />
-            <img className={styles.logo} src="twitter.svg" alt="twitter" />
-            <img className={styles.logo} src="linkedin.svg" alt="linkedin" />
+          <div className="mt-16 flex">
+            {icons.map(({ icon, label }) => (
+              <img
+                key={label}
+                className="w-20 h-20 mr-8"
+                src={icon}
+                alt={label}
+              />
+            ))}{' '}
           </div>
         </section>
-        <img className={styles.image} src="face-image.svg" alt="unsplash" />
+        <img
+          className="w-[700px] -ml-24 -mt-32 -mr-40"
+          src="face-image.svg"
+          alt="unsplash"
+        />
       </main>
     </div>
   );
