334b97e8fffd3eba7e2541a7216629b00f468a6f
Bryan-Cee <cheruiyotbryan@gmail.com>
2023-11-01 19:25:11 +0300

    setup deploy

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,12 +13,36 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            Here are some of the projects I have worked on over the years.
           </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
+            {projectsData.projects.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Design Playground
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            I like to play around with frontend designs and keep my knowledge up to date. Here are
+            some of the work I have done.
+          </p>
+        </div>
+        <div className="container py-12">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.designs.map((d) => (
               <Card
                 key={d.title}
                 title={d.title}
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,20 +1,30 @@
-const projectsData = [
-  {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+const projectsData = {
+  projects: [
+    {
+      title: 'The Time Machine',
+      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
-]
+      imgSrc: '/static/images/time-machine.jpg',
+      href: '/blog/the-time-machine',
+    },
+    {
+      title: 'Bonsai',
+      description: `The freelancing market is booming with the new age of remote work.
+    This project is a sample page for the pricing of the services provided by Bonsai.`,
+      imgSrc: '/static/images/bonsai.jpg',
+      href: 'https://www.frontendpractice.com/projects/bonsai',
+    },
+  ],
+  designs: [
+    {
+      title: 'Bonsai',
+      description: `The freelancing market is booming with the new age of remote work.
+    This project is a sample page for the pricing of the services provided by Bonsai.`,
+      imgSrc: '/static/images/bonsai.jpg',
+      href: 'https://www.frontendpractice.com/projects/bonsai',
+    },
+  ],
+}
 
 export default projectsData
