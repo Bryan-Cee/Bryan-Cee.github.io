ba4e4d55900c0c0b61dec5de66856583cf3e549c
Bryan-Cee <cheruiyotbryan@gmail.com>
2024-04-25 14:20:28 +0300

    add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,22 @@
 const projectsData = {
   projects: [
     {
-      title: 'The Time Machine',
-      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-      imgSrc: '/static/images/time-machine.jpg',
-      href: '/blog/the-time-machine',
+      title: 'acelr8',
+      description: `Accelerate your hiring. Onboard experienced Talent Partners to your team, within a week!`,
+      imgSrc: '/static/images/www.acelr8.com_.png',
+      href: 'https://www.acelr8.com/',
     },
     {
-      title: 'Bonsai',
-      description: `The freelancing market is booming with the new age of remote work.
-    This project is a sample page for the pricing of the services provided by Bonsai.`,
-      imgSrc: '/static/images/bonsai.jpg',
-      href: 'https://www.frontendpractice.com/projects/bonsai',
+      title: 'ofcolor',
+      description: `Support employees of color. Secure the future of your business. OfColor is a black-owned financial wellness platform that is available to all—but unapologetically centered around employees of color...`,
+      imgSrc: '/static/images/www.ofcolor.com_.png',
+      href: 'https://www.ofcolor.com/',
+    },
+    {
+      title: 'finexapp',
+      description: `Financial tracking app. Track your accounts as you spend money easily.`,
+      imgSrc: '/static/images/finexapp.vercel.app_.png',
+      href: 'https://finexapp.vercel.app/',
     },
   ],
   designs: [
